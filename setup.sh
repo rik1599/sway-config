33c0006768ca6f4daeb3070b3eb37e5ae1af8387
Riccardo Belliato <Riccardo Belliato>
2024-02-28 12:03:15 +0100

    add outputs to setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #
 # Packages installs
 #
+echo "Installing packages"
 sudo apt update -y
 sudo apt upgrade -y
 
@@ -40,23 +41,27 @@ sudo apt install -y --no-install-recommends \
 	pavucontrol
 
 #
-# Add users to groups
+# Add user to groups
 #
+echo "Add user to groups"
 sudo usermod -a -G input, lpadmin $USER
 
 #
 # Start services
 #
+echo "Start services"
 sudo systemctl enable cups --now
 
 #
 # Install configuration files
 #
+echo "Config files"
 git clone https://github.com/rik1599/sway-config.git ~/.config
 
 #
 # Neovim
 #
+echo "Neovim install"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/bin/nvim
@@ -64,6 +69,7 @@ sudo mv nvim.appimage /usr/bin/nvim
 #
 # Font Awesome 6
 #
+echo "Font Awesome install"
 mkdir -p ~/.local/share/fonts
 curl https://use.fontawesome.com/releases/v6.5.1/fontawesome-free-6.5.1-desktop.zip \
 	-o fontawesome.zip
@@ -74,6 +80,7 @@ rm fontawesome.zip
 #
 # Adw-gtk3
 #
+echo "Adw-gtk3 install"
 mkdir -p .local/share/themes
 curl -L https://github.com/lassekongo83/adw-gtk3/releases/download/v5.2/adw-gtk3v5-2.tar.xz \
 	-o adw-gtk3.tar.xz
@@ -83,17 +90,20 @@ rm adw-gtk3.tar.xz
 #
 # Flathub setup
 #
+echo "Flathub setup"
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 #
 # Oh my zsh
 #
+echo "Install Oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo "source ~/.config/sway/start" >> .zshrc 
 
 #
 # Change shell to zsh
 #
+echo "Changing shell"
 chsh $USER -s /usr/bin/zsh
 
 #
